d575ccb4a765e3da1b7775d42a2d7278252500d9
Ali <abdelaaliaitbrahim@gmail.com>
2022-05-17 23:27:31 +0100

    add comment

--- appli/urls.py
+++ appli/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+#This is a dummy comment
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('', include('blog.urls')),
